089e92b1d34b33c954c19236d4cd9c0682ce93f0
CamillaKleymen <camillakleymen@outlook.com>
2024-10-26 17:41:37 +0500

    page almost done

--- js/script.js
+++ js/script.js
@@ -18,34 +18,35 @@ $(document).ready(function(){
     });
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    // Находим все дропдауны на странице
-    var dropdowns = document.querySelectorAll('.dropdown');
 
-    dropdowns.forEach(function(dropdown) {
-        var button = dropdown.querySelector('.dropdown-toggle');
-        var menu = dropdown.querySelector('.dropdown-menu');
 
-        // Обработчик клика по кнопке дропдауна
-        button.addEventListener('click', function(event) {
-            event.stopPropagation();
-            menu.classList.toggle('show');
-        });
 
-        // Закрытие дропдауна при клике вне его
-        document.addEventListener('click', function(event) {
-            if (!dropdown.contains(event.target)) {
-                menu.classList.remove('show');
-            }
-        });
 
-        // Обработка выбора элемента из дропдауна
-        var items = menu.querySelectorAll('.dropdown-item');
-        items.forEach(function(item) {
-            item.addEventListener('click', function() {
-                button.textContent = this.textContent;
-                menu.classList.remove('show');
-            });
-        });
-    });
-});
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     var dropdowns = document.querySelectorAll('.dropdown');
+
+//     dropdowns.forEach(function(dropdown) {
+//         var button = dropdown.querySelector('.dropdown-toggle');
+//         var menu = dropdown.querySelector('.dropdown-menu');
+
+//         button.addEventListener('click', function(event) {
+//             event.stopPropagation();
+//             menu.classList.toggle('show');
+//         });
+
+//         document.addEventListener('click', function(event) {
+//             if (!dropdown.contains(event.target)) {
+//                 menu.classList.remove('show');
+//             }
+//         });
+
+//         var items = menu.querySelectorAll('.dropdown-item');
+//         items.forEach(function(item) {
+//             item.addEventListener('click', function() {
+//                 button.textContent = this.textContent;
+//                 menu.classList.remove('show');
+//             });
+//         });
+//     });
+// });
